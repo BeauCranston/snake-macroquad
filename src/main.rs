d2e20119cb744bc3564bceeb27a56e220016f15e
beau <beau2tie@gmail.com>
2025-10-03 23:58:36 -0400

    created new cargo project

---
+++ src/main.rs
@@ -0,0 +1,16 @@
+use macroquad::prelude::*;
+
+#[macroquad::main("Snake")]
+async fn main() {
+    loop {
+        clear_background(BLACK);
+
+        //handle input
+
+        //update logic
+
+        //draw
+
+        next_frame().await
+    }
+}
