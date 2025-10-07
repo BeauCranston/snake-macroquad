030330f8088e7b4e5b7182c48e4b420cc42f1f18
beau <beau2tie@gmail.com>
2025-10-07 19:37:38 -0400

    implemented self eating logic

--- src/main.rs
+++ src/main.rs
@@ -27,23 +27,41 @@ fn draw_fruit(grid: &GameGrid, point: Point) {
 }
 
 fn update(snake: &mut Snake, current_fruit_point: &mut Point, score: &mut i32) {
+    // save current head in variable
     let old_head = snake.head.clone();
+    //move snake head to new position
     snake.head = (snake.head.0 + snake.dir.0, snake.head.1 + snake.dir.1);
+    //push old head to body
     snake.body.push_front(old_head);
     let eating_fruit =
         snake.head.0 == current_fruit_point.0 && snake.head.1 == current_fruit_point.1;
     if !eating_fruit {
+        //pop off end of tail
         snake.body.pop_back();
     } else {
+        //if fruit has been eaten, grow size by not popping tail
         *score += 1;
         current_fruit_point.0 = 0;
         current_fruit_point.1 = 0;
         println!("{} score", *score);
     }
 }
+fn check_self_eat(snake: &Snake) -> bool {
+    let mut eating_self = false;
+    for node in &snake.body {
+        if snake.head.0 == node.0 && snake.head.1 == node.1 {
+            eating_self = true;
+        }
+    }
+    eating_self
+}
 fn check_inbounds(snake: &Snake, grid: &GameGrid) -> bool {
     return (0.0 <= snake.head.0 as f32 && grid.grid_size > snake.head.0 as f32)
         && (0.0 <= snake.head.1 as f32 && grid.grid_size > snake.head.1 as f32);
+}
+
+fn fruit_has_been_eaten(current_fruit_point: &Point) -> bool {
+    current_fruit_point.0 == 0 && current_fruit_point.1 == 0
 }
 
 #[macroquad::main(conf)]
@@ -65,13 +83,13 @@ async fn main() {
             if get_time() - last_movement_update > speed {
                 println!("{} {} current dir", dir.0, dir.1);
                 update(&mut snake, &mut current_fruit_point, &mut score);
-                if !check_inbounds(&snake, &grid) {
+                if !check_inbounds(&snake, &grid) || check_self_eat(&snake) {
                     game_over = true;
                 }
                 last_movement_update = get_time();
             }
             if get_time() - last_fruit_spawn > food_spawn_rate {
-                if current_fruit_point.0 == 0 && current_fruit_point.1 == 0 {
+                if fruit_has_been_eaten(&current_fruit_point) {
                     current_fruit_point = spawn_fruit();
                 }
                 last_fruit_spawn = get_time();
@@ -79,7 +97,9 @@ async fn main() {
             clear_background(WHITE);
             snake.draw(&grid);
             grid.draw();
-            draw_fruit(&grid, current_fruit_point);
+            if !fruit_has_been_eaten(&current_fruit_point) {
+                draw_fruit(&grid, current_fruit_point);
+            }
         }
 
         next_frame().await
